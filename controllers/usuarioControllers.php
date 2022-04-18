7703df0a333840d7bc13bd35d34f85e7d5b2d19d
santiago clemenzi <clemenzisantiago@gmail.com>
2022-04-18 11:25:50 -0300

    form registrarse listo

--- controllers/usuarioControllers.php
+++ controllers/usuarioControllers.php
@@ -3,4 +3,14 @@ class usuarioControllers{
     public function index(){
         echo 'Controlador usuario funcion publica index()';
     }
+
+    public function registro(){
+        require_once'views/usuario/registro.php';
+    }
+
+    public function save(){
+        if(isset($_POST)){
+            var_dump($_POST);
+        }
+    }
 }
