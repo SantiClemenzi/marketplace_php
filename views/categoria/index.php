4d437dbd39738522856887614056387b79a35a9b
santiago clemenzi <clemenzisantiago@gmail.com>
2022-04-20 20:23:47 -0300

    tabla categorias lista

--- views/categoria/index.php
+++ views/categoria/index.php
@@ -1,4 +1,17 @@
 <h1>Gestionar Categorias</h1>
-<?php while ($cat = $categorias->fetch_object()) : ?>
-    <?= $cat->nombre; ?></br>
-<?php endwhile; ?>
+
+<a class="button button-small" href="#">
+    Crear categoria
+</a>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+    </tr>
+    <?php while ($cat = $categorias->fetch_object()) : ?>
+        <tr>
+            <td><?= $cat->id; ?></td>
+            <td><?= $cat->nombre; ?></td>
+        </tr>
+    <?php endwhile; ?>
+</table>
--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -14,7 +14,7 @@
                <?php endif; ?>
                <ul>
                    <?php if (isset($_SESSION['admin'])) : ?>
-                       <li><a href="#">Gestionar categorias</a></li>
+                       <li><a href="http://localhost/projects/master_PHP/marketplace/categoriaControllers/index">Gestionar categorias</a></li>
                        <li><a href="#">Gestionar productos</a></li>
                        <li><a href="#">Gestionar pedidos</a></li>
                    <?php endif; ?>
