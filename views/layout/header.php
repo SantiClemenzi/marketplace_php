c4edf4f2822ec02e5dddf4658aad0772557b6f5c
santiago clemenzi <clemenzisantiago@gmail.com>
2022-04-20 09:17:22 -0300

    estilos funcionandos sin la base_url

--- views/layout/header.php
+++ views/layout/header.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="http://localhost/projects/master_PHP/marketplace/assets/css/styles.css">
     <title>Marketplace</title>
 </head>
 
@@ -13,7 +12,7 @@
     <!-- header -->
     <header id="header">
         <div id="icon">
-            <img src="assets/img/camiseta.png" alt="Camiseta logo" />
+            <img src="http://localhost/projects/master_PHP/marketplace/assets/img/camiseta.png" alt="Camiseta logo" />
             <a href="index.php">
                 <h1>
                     Shirts market
