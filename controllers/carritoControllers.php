efb0be74c334d715993dfe6dccfa0d127fe52ad6
santiago clemenzi <clemenzisantiago@gmail.com>
2022-04-26 10:02:49 -0300

    funcion add creada

--- controllers/carritoControllers.php
+++ controllers/carritoControllers.php
@@ -1,1 +1,55 @@
 <?php
+require_once 'models/producto.php';
+
+class carritoControllers
+{
+    public function index()
+    {
+        echo 'accion INDEX del carrito';
+        var_dump($_SESSION['carrito']);
+    }
+    public function add()
+    {
+        // comprobamos que el id este definido
+        if (isset($_GET['id'])) {
+            $producto_id = $_GET['id'];
+        } else {
+            header('Location: http://localhost/projects/master_PHP/marketplace');
+        }
+
+        // comprobamos que carrito este definido 
+        $counter = 0;
+
+        if (isset($_SESSION['carrito'])) {
+            foreach ($_SESSION['carrito'] as $index => $elemento) {
+                if ($elemento['id_producto'] == $producto_id) {
+                    $_SESSION['carrito'][$index]['unidades']++;
+                    $counter++;
+                }
+            }
+        }
+        if (!isset($_SESSION['carrito']) || $counter == 0) {
+            $producto = new producto();
+            $producto->setId($producto_id);
+            $producto = $producto->getOne();
+
+            // añadir producto al carrito
+            if (is_object($producto)) {
+                $_SESSION['carrito'][] = array(
+                    "id_producto" => $producto->id,
+                    "precio" => $producto->precio,
+                    "unidades" => 1,
+                    "producto" => $producto,
+                );
+            }
+        }
+    }
+    public function remove()
+    {
+    }
+    public function delete_all()
+    {
+        unset($_SESSION['carrito']);
+        header('Location: http://localhost/projects/master_PHP/marketplace');
+    }
+}
--- views/producto/ver.php
+++ views/producto/ver.php
@@ -1,19 +1,19 @@
-<?php if (isset($product)): ?>
+<?php if (isset($product)) : ?>
 	<h1><?= $product->nombre ?></h1>
 	<div id="detail-product">
 		<div class="image">
-			<?php if ($product->imagen != null): ?>
+			<?php if ($product->imagen != null) : ?>
 				<img src="http://localhost/projects/master_PHP/marketplace/uploads/images/<?= $product->imagen ?>" />
-			<?php else: ?>
+			<?php else : ?>
 				<img src="http://localhost/projects/master_PHP/marketplace/assets/img/camiseta.png" />
 			<?php endif; ?>
 		</div>
 		<div class="data">
 			<p class="description"><?= $product->descripcion ?></p>
 			<p class="price"><?= $product->precio ?>$</p>
-			<a href="#" class="button">Comprar</a>
+			<a href="http://localhost/projects/master_PHP/marketplace/carritoControllers/add/<?= $product->id ?>" class="button">Comprar</a>
 		</div>
 	</div>
-<?php else: ?>
+<?php else : ?>
 	<h1>El producto no existe</h1>
 <?php endif; ?>
